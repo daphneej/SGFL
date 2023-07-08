b419aaec52133dc5eb6b185271bd87a6edbcb9a8
daphneej <josephdaphnee975@gmail.com>
2023-07-08 17:58:45 -0400

    Protect user routes

--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -9,15 +9,17 @@ import {
   deleteUser,
 } from "../controllers/userController.js";
 
+import { protectRoutes } from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
 
 // Define routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/logout", logOutUser);
-router.get("/profile", getUser);
-router.put("/update", updateUser);
-router.delete("/delete", deleteUser);
+router.post("/logout", protectRoutes, logOutUser);
+router.get("/profile", protectRoutes, getUser);
+router.put("/update", protectRoutes, updateUser);
+router.delete("/delete", protectRoutes, deleteUser);
 
 // Export the router
 export { router as usersRouter };
