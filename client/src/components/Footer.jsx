d0b5ac859d729889a9a26f0ef118ce5d379a5782
daphneej <josephdaphnee975@gmail.com>
2023-07-21 21:10:19 -0400

    Adding courses

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="flex justify-center items-center p-2 bg-black text-white">
-      <span className="text-center">
+      <span className="text-center text-xs">
         Copyright &copy; 2023-2024 - Hard&Soft - Tous droits reservés
       </span>
     </footer>
